7caa4fb57773578dc9867adcf18f2452be1737cb
Edwin Jorge <ed.jorge1122@gmail.com>
2022-10-17 16:27:54 -0500

    crud

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,108 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+
+const API = 'http://localhost:3001'
 
 function App () {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])
+  const [task, setTask] = useState('')
+  const [popupActive, setPopupActive] = useState(false)
+
+  useEffect(() => {
+    getTodos()
+  }, [])
+
+  const getTodos = () => {
+    fetch(API + '/todos').then(res => res.json()).then(data => setTodos(data)).catch(err => console.log(err))
+  }
+
+  const addTodo = async () => {
+    const data = await fetch(API + '/todo/new', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        text: task
+      })
+    }).then(res => res.json())
+
+    setTodos([...todos, data])
+
+    setPopupActive(false)
+    setTask('')
+  }
+
+  const completeTodo = async (id) => {
+    const data = await fetch(API + '/todo/complete/' + id).then(res => res.json())
+
+    setTodos((todos) => todos.map((todo) => {
+      if (todo._id === data._id) {
+        todo.done = data.done
+      }
+
+      return todo
+    }))
+  }
+
+  const deleteTodo = async (id) => {
+    const data = await fetch(API + '/todo/delete/' + id, {
+      method: 'DELETE'
+    }).then(res => res.json)
+
+    setTodos(todos => todos.filter((todo) => todo._id !== data.result._id))
+  }
 
   return (
     <div className='App'>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src='/vite.svg' className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://reactjs.org' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
+      <h1>Welcome</h1>
+      <h4>Your tasks</h4>
+
+      <div className='todos'>
+        {
+          todos.length > 0 ? (
+            todos.map(todo => (
+              <div
+                className={'todo ' + (todo.done ? 'is-complete' : '')}
+                key={todo._id}
+                onClick={() => completeTodo(todo._id)}
+              >
+                <div className='checkbox' />
+
+                <div className='text'>{todo.text}</div>
+                <div onClick={() => deleteTodo(todo._id)} className='delete-todo'>
+                  X
+                </div>
+              </div>
+            ))
+          ) : (
+            'No tasks yet'
+          )
+        }
       </div>
-      <h1>To do APP</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div onClick={() => setPopupActive(true)} className='addPopup'>
+        +
       </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
+
+      {
+        popupActive
+          ? (
+          <div className='popup'>
+              <div onClick={() => setPopupActive(false)} className='closePopup'>X</div>
+            <div className='content'>
+              <h3>Add Task</h3>
+                <input type='text' className='add-todo-input' placeholder='Write your tasks...' onChange={(e) => setTask(e.target.value)} 
+              />
+
+              <div onClick={addTodo} className='button'>
+                Create Task
+              </div>
+            </div>
+          </div>
+        ) : (
+            ''
+        )
+      }
     </div>
   )
 }
